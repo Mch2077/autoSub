82d0dd7079a5480763eb3300f0dc4bfa37171de5
Ethan Chain <mig.chen2077@gmail.com>
2020-05-15 14:15:09 +0800

    简单视频上传实现

---
+++ src/main/java/awesome/team/controller/VideoController.java
@@ -0,0 +1,47 @@
+package awesome.team.controller;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequestMapping("/video")
+@RestController
+public class VideoController {
+	@RequestMapping(value = "upload", method = RequestMethod.POST)
+	    @ResponseBody
+	    public Map<String,String> savaVideo(@RequestParam("file") MultipartFile file)
+	            throws IllegalStateException {
+	        Map<String,String> resultMap = new HashMap<>();
+	        try{
+	            //获取文件后缀
+	            String fileExt = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".") + 1)
+	                    .toLowerCase();
+	            // 重构文件名称
+	            String pikId = UUID.randomUUID().toString().replaceAll("-", "");
+	            String newVidoeName = pikId + "." + fileExt;
+	            //保存视频
+	            File fileSave = new File("/home/mig-chen/文档/data/videos", newVidoeName);
+	            file.transferTo(fileSave);
+	            resultMap.put("resCode","1");
+	            resultMap.put("webShowPath","/home/mig-chen/文档/data/videos" + newVidoeName);
+	
+	            return  resultMap;
+	
+	        }catch (Exception e){
+	            e.printStackTrace();
+	            resultMap.put("resCode","0");
+	            return  resultMap ;
+	
+	        }
+	    }
+	
+}
+
