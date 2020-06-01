620d9b1f49ee379ca6dc400350feda9d79714f96
SoAl1ve <lk849968467@gmail.com>
2020-06-01 17:20:40 +0800

    更新：Springboot+Quartz 实现定时任务计划

---
+++ src/main/java/awesome/team/util/ClearCatchUtil.java
@@ -0,0 +1,36 @@
+package awesome.team.util;
+
+import java.io.File;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.util.ClassUtils;
+
+public class ClearCatchUtil extends QuartzJobBean {
+	private String path = ClassUtils.getDefaultClassLoader().getResource("static/res").getPath()+"/";
+    @Override
+    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+    	delFile(path);
+    }
+    
+    private void delFile(String path){
+		File file = new File(path);
+		if (file.exists()) {
+			String[] tempList = file.list();
+			File temp = null;
+			for (int i = 0; i < tempList.length; i++) {
+				if (path.endsWith(File.separator)) {
+					temp = new File(path + tempList[i]);
+				} else {
+					temp = new File(path + File.separator + tempList[i]);
+				}
+				if (temp.isFile()) {
+					temp.delete();
+ 
+				}
+ 
+			}
+ 
+		}
+	}
+}
---
+++ src/main/java/awesome/team/util/QuartzConfig.java
@@ -0,0 +1,49 @@
+package awesome.team.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+@Configuration
+public class QuartzConfig {
+    /**
+     * 创建Job对象
+     */
+    @Bean
+    public JobDetailFactoryBean jobDetailFactoryBean() {
+        JobDetailFactoryBean factory = new JobDetailFactoryBean();
+        //关联我们自己的Job类
+        factory.setJobClass(ClearCatchUtil.class);
+        return factory;
+    }
+
+    /**
+     * 创建Trigger对象
+     */
+    /**
+     * Cron Trigger
+     */
+    @Bean
+    public CronTriggerFactoryBean cronTriggerFactoryBean(JobDetailFactoryBean jobDetailFactoryBean) {
+        CronTriggerFactoryBean factory = new CronTriggerFactoryBean();
+        factory.setJobDetail(jobDetailFactoryBean.getObject());
+        //定义定时表达式,每日凌晨1点清除指定文件夹下的缓存
+        factory.setCronExpression("0 0 1 * * ?");
+        return factory;
+    }
+    /**
+     * 创建Scheduler对象
+     */
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean(CronTriggerFactoryBean cronTriggerFactoryBean, MyAdaptableJobFactory myAdaptableJobFactory) {
+        SchedulerFactoryBean factory = new SchedulerFactoryBean();
+        //关联trigger
+        factory.setTriggers(cronTriggerFactoryBean.getObject());
+        factory.setJobFactory(myAdaptableJobFactory);
+        return factory;
+    }
+
+}
+
