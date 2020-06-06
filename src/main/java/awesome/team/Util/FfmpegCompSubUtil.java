d1f6aa53a3a294fee901b71a9b117286003e922d
luowei <2468229259@qq.com>
2020-06-06 19:26:09 +0800

    修正双语显示问题

--- src/main/java/awesome/team/Util/FfmpegCompSubUtil.java
+++ src/main/java/awesome/team/Util/FfmpegCompSubUtil.java
@@ -73,16 +73,22 @@ public class FfmpegCompSubUtil {
     	return AM0;
     }
     
-    //将字符串写入srt文件（中文）
+    //将字符串写入srt文件
 	private static void Srt(List<Map<String,String>> AMap, File file) {
 		try {
-		char[] fh= new char[10];
-		fh[0]='，';fh[1]='。';fh[2]='、';fh[3]='？';fh[4]='！';fh[5]='：';fh[6]='；';fh[7]='）';fh[8]='”';fh[9]='》';
+		int EngORCh;
+		char[] fh1= new char[10];
+		fh1[0]='，';fh1[1]='。';fh1[2]='、';fh1[3]='？';fh1[4]='！';fh1[5]='：';fh1[6]='；';fh1[7]='）';fh1[8]='”';fh1[9]='》';
+		String sm = "'";
+		char[] fh2= new char[8];
+		fh2[0]=',';fh2[1]='.';fh2[2]='?';fh2[3]='!';fh2[4]=':';fh2[5]=';';fh2[6]=')';fh2[7]='"';
         FileOutputStream fw = new FileOutputStream(file); 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fw,"UTF-8")); 
         for(int i=0; i<AMap.size(); i++ ) {
         	Map m = AMap.get(i);
         	int a = 30;
+        	int b = 60;
+        	int c = 0;
         	String s = "-->";
         	String s1 = m.get("bg").toString(); 
         	String s2 = m.get("ed").toString();
@@ -96,90 +102,117 @@ public class FfmpegCompSubUtil {
         	bw.write(" ");
         	bw.write(s2);
         	bw.newLine();//换行 
-        	for(int j=0;j<m.get("onebest").toString().length()-1;j=j+a) {
+        	for(int j=0;j<m.get("onebest").toString().length()-1;j=j+c) {
+        		int n = 1;
         		if(a!=30) a=30;
+        		if(b!=60) b=60;
+        		char cha = m.get("onebest").toString().charAt(j),sh;
+        		if(cha==' ') {
+        			sh=cha;
+        		    while (sh==' ') { 
+        			    sh=m.get("onebest").toString().charAt(j+n);
+        			    n++;
+        		      }
+        		    if((sh>='a'&&sh<='z')||(sh>='A'&&sh<='Z')) EngORCh=1;
+        		    else EngORCh=0;
+        		}
+        		else if((cha>='a'&&cha<='z')||(cha>='A'&&cha<='Z')) EngORCh=1;
+    		    else EngORCh=0;
+        		if(EngORCh==0) {
         		if(j+a+1<m.get("onebest").toString().length()) {
+        			System.out.println(j);
+        			char x=m.get("onebest").toString().charAt(j+a);
+        			while((x>='a'&&x<='z')||(x>='A'&&x<='Z')||x==fh2[0]||x==fh2[1]||x==fh2[2]||x==fh2[3]||x==fh2[4]||x==fh2[5]||x==fh2[6]||x==fh2[7]||x==' '||x==sm.charAt(0)) {
+        				a--;
+        				x=m.get("onebest").toString().charAt(j+a);
+        			}
         			for(int k=0;k<10;k++){
-        			if(m.get("onebest").toString().charAt(j+a) == fh[k]) a=a+1;
+        			if(m.get("onebest").toString().charAt(j+a) == fh1[k]) a=a+1;
         			}
         			s3=m.get("onebest").toString().substring(j,j+a);
             	    bw.write(s3);
             	    bw.newLine();//换行 
         		}
         		else {
+        			int end=0;
+        			char x=m.get("onebest").toString().charAt(m.get("onebest").toString().length()-1);
+        			a=m.get("onebest").toString().length()-1;
+        			while((x>='a'&&x<='z')||(x>='A'&&x<='Z')||x==fh2[0]||x==fh2[1]||x==fh2[2]||x==fh2[3]||x==fh2[4]||x==fh2[5]||x==fh2[6]||x==fh2[7]||x==' '||x==sm.charAt(0)) {
+        				a--;
+        				x=m.get("onebest").toString().charAt(j+a);
+        				end=1;
+        			}
+        			if(end==0) {
         			s3=m.get("onebest").toString().substring(j,m.get("onebest").toString().length());
         			bw.write(s3);
+        			}
+        			else if(end==1) {
+        				a++;
+        				s3=m.get("onebest").toString().substring(j,j+a);
+        			bw.write(s3);
+        			bw.newLine();//换行 
+        			}
         		}
-        	}
-        	bw.newLine();//换行 
-        	if(i != AMap.size()) bw.newLine();//换行 
-        	}
-        bw.close();
-		}catch(Exception e){  
-			System.out.println(e.toString());  
-        } 
-	}
-	
-    //将字符串写入srt文件（英文）
-	private static void ESrt(List<Map<String,String>> AMap, File file) {
-		try {
-		String sm = "'";
-		char[] fh= new char[8];
-		fh[0]=',';fh[1]='.';fh[2]='?';fh[3]='!';fh[4]=':';fh[5]=';';fh[6]=')';fh[7]='"';
-        FileOutputStream fw = new FileOutputStream(file); 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fw,"UTF-8")); 
-        for(int i=0; i<AMap.size(); i++ ) {
-        	Map m = AMap.get(i);
-        	int a = 60;
-        	String s = "-->";
-        	String s1 = m.get("bg").toString(); 
-        	String s2 = m.get("ed").toString();
-        	String s3 = "";
-        	String s4 = m.get("sn").toString();
-        	bw.write(s4);
-        	bw.newLine();//换行  
-        	bw.write(s1); 
-        	bw.write(" ");
-        	bw.write(s);
-        	bw.write(" ");
-        	bw.write(s2);
-        	bw.newLine();//换行 
-        	for(int j=0;j<m.get("onebest").toString().length()-1;j=j+a) {
-        		if(a!=60) a=60;
-        		if(j+a+1<m.get("onebest").toString().length()) {
-        			char c = m.get("onebest").toString().charAt(j+a-1);
-        			char c1 = m.get("onebest").toString().charAt(j+a);
-        			if((c1>='a'&&c1<='z')||(c1>='A'&&c1<='Z')) {
-        				if(c!=sm.charAt(0)) {
-        			    s3=m.get("onebest").toString().substring(j,j+a)+'-';
-            	        bw.write(s3);
-            	        bw.newLine();//换行 
-        				}
-        				else {
-        					a++;
-            			    s3=m.get("onebest").toString().substring(j,j+a);
-                	        bw.write(s3);
-                	        bw.newLine();//换行 
-        				}
-        			}
-        			else if(c1==sm.charAt(0)){
-        				a = a+2;
-        			    s3=m.get("onebest").toString().substring(j,j+a);
-            	        bw.write(s3);
-            	        bw.newLine();//换行 
-        					}		
-        			else {
-        			    for(int k=0;k<8;k++){
-        			       if(m.get("onebest").toString().charAt(j+a) == fh[k]) a=a+1;  
-        				}
-        			    s3=m.get("onebest").toString().substring(j,j+a);
-            	        bw.write(s3);
-            	        bw.newLine();//换行 
-        			}
+        		c=a;
         		}
         		else {
-        			s3=m.get("onebest").toString().substring(j,m.get("onebest").toString().length());
-        			bw.write(s3);
+            		if(j+b+1<m.get("onebest").toString().length()) {
+            			char x=m.get("onebest").toString().charAt(j+b);
+            			while((x<'a'||x>'z'||x==' ')&&(x<'A'||x>'Z'||x==' ')&&x!=fh2[0]&&x!=fh2[1]&&x!=fh2[2]&&x!=fh2[3]&&x!=fh2[4]&&x!=fh2[5]&&x!=fh2[6]&&x!=fh2[7]&&x!=sm.charAt(0)) {
+            				b--;
+            				x=m.get("onebest").toString().charAt(j+b);
+            			}
+            			char c1 = m.get("onebest").toString().charAt(j+b-1);
+            			char c2 = m.get("onebest").toString().charAt(j+b);
+            			if((c2>='a'&&c2<='z')||(c2>='A'&&c2<='Z')) {
+            				if(c1!=sm.charAt(0)&&(c1>='a'&&c1<='z')||(c1>='A'&&c1<='Z')){
+            					s3=m.get("onebest").toString().substring(j,j+a)+'-';
+                     	        bw.write(s3);
+                     	        bw.newLine();//换行
+            				}
+            				else {
+            					b++;
+                			    s3=m.get("onebest").toString().substring(j,j+b);
+                    	        bw.write(s3);
+                    	        bw.newLine();//换行 
+            				}
+            			}
+            			else if(c2==sm.charAt(0)){
+            				b = b+2;
+            			    s3=m.get("onebest").toString().substring(j,j+b);
+                	        bw.write(s3);
+                	        bw.newLine();//换行 
+            					}		
+            			else {
+            			    for(int k=0;k<8;k++){
+            			       if(m.get("onebest").toString().charAt(j+b) == fh2[k]) b=b+1;  
+            				}
+            			    s3=m.get("onebest").toString().substring(j,j+b);
+                	        bw.write(s3);
+                	        bw.newLine();//换行 
+            			}
+            		}
+            		else {
+            			int end=0;
+            			char x=m.get("onebest").toString().charAt(m.get("onebest").toString().length()-1);
+            			b=m.get("onebest").toString().length()-1;
+            			while((x<'a'||x>'z'||x==' ')&&(x<'A'||x>'Z'||x==' ')&&x!=fh2[0]&&x!=fh2[1]&&x!=fh2[2]&&x!=fh2[3]&&x!=fh2[4]&&x!=fh2[5]&&x!=fh2[6]&&x!=fh2[7]&&x!=sm.charAt(0)) {
+            				b--;
+            				x=m.get("onebest").toString().charAt(j+b);
+            				end=1;
+            			}
+            			if(end==0) {
+            			s3=m.get("onebest").toString().substring(j,m.get("onebest").toString().length());
+            			bw.write(s3);
+            			}
+            			else if(end==1) {
+            				b++;
+            				s3=m.get("onebest").toString().substring(j,j+b);
+            			bw.write(s3);
+            			bw.newLine();//换行 
+            			}
+            		}
+            		c=b;
         		}
         	}
         	bw.newLine();//换行 
@@ -202,23 +235,12 @@ public class FfmpegCompSubUtil {
         String subtitleUrl = filename + ".srt";
     	
     	File subtitleFile = new File(subtitleUrl);
-    	//判断字幕类型
-    	int n=0,n1=0;
-        for(int i=0; i<AMap.size(); i++ ) {
-        	Map m = AMap.get(i);
-        	String s = m.get("sn").toString();
-        	n1=n1+s.length();
-        	for(int j=0;j<s.length();j++) {
-        		char c =s.charAt(j);
-        		if((c>='a'&&c<='z')||(c>='A'&&c<='Z')) n++;
-        	}
-        }
+    	
         //字幕写入
-        if(n > n1-n) ESrt(AMap,subtitleFile);
-        else Srt(AMap,subtitleFile);
+        Srt(AMap,subtitleFile);
         
         String burnedFile = "";
-        String burnedFile1= "";
+        String burnedFile1 = "";
         burnedFile1 = Wfilename+"(字幕版)."+fileSuffix;
         try {
             //将字幕压缩至视频中
